fea9e84735241d61f7170d93d36559557c637534
tecyang <me@tecyang.com>
2023-02-20 10:25:11 +0800

    feat: init

---
+++ StockMenuTools/AddStockWindow.swift
@@ -0,0 +1,15 @@
+//
+//  AddStockWindow.swift
+//  MenuToolDemo
+//
+//  Created by tecyang on 2023/2/14.
+//  Copyright © 2023 5km. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+
+class AddStockWindow : NSWindowController{
+    
+}
